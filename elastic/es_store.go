e142ee39bd9475e462c1345a12784ace662cc9bb
Sebastian Mancke <s.mancke@tarent.de>
2018-02-22 20:56:17 +0100

    some refactorings

--- elastic/es_store.go
+++ elastic/es_store.go
@@ -2,7 +2,6 @@ package elastic
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	es "github.com/olivere/elastic"
 	"github.com/snabble/go-jstore"
@@ -16,7 +15,7 @@ type ElasticStore struct {
 	client *es.Client
 }
 
-func NewElasticStore(baseURL string) (jstore.JStore, error) {
+func NewElasticStore(baseURL string) (jstore.Store, error) {
 	client, err := es.NewClient(es.SetURL(baseURL))
 	return &ElasticStore{
 		client: client,
@@ -24,31 +23,28 @@ func NewElasticStore(baseURL string) (jstore.JStore, error) {
 }
 
 func (store *ElasticStore) Delete(project, documentType, id string) error {
-	_, err := store.client.Delete().Index(project).Type(documentType).Id(id).Do(store.cntx())
+	_, err := store.client.Delete().
+		Refresh("true").
+		Index(project).
+		Type(documentType).
+		Id(id).
+		Do(store.cntx())
 	return err
 }
 
 func (store *ElasticStore) Save(project, documentType, id string, json string) error {
-	_, err := store.client.Index().Index(project).Type(documentType).Id(id).BodyString(json).Do(store.cntx())
+	_, err := store.client.Index().
+		Index(project).
+		Refresh("true").
+		Type(documentType).
+		Id(id).
+		BodyString(json).
+		Do(store.cntx())
 	return err
 }
 
 func (store *ElasticStore) Find(project, documentType string, matcher ...jstore.Matcher) (string, error) {
-	boolQuery := es.NewBoolQuery()
-	for _, m := range matcher {
-		switch m := m.(type) {
-		case jstore.IdMatcher:
-			boolQuery.Must(es.NewIdsQuery().Ids(m.Value))
-		case jstore.EqMatcher:
-			if _, isString := m.Value.(string); isString {
-				boolQuery.Must(es.NewTermQuery(m.Property+".keyword", m.Value))
-			} else {
-				boolQuery.Must(es.NewTermQuery(m.Property, m.Value))
-			}
-		}
-	}
-
-	resp, err := store.client.Search(project).Type(documentType).Index(project).Query(boolQuery).Do(store.cntx())
+	resp, err := store.createSearch(project, documentType, matcher...).Do(store.cntx())
 	if err != nil {
 		return "", err
 	}
@@ -64,26 +60,27 @@ func (store *ElasticStore) FindN(project, documentType string, maxCount int, mat
 	return []string{}, errors.New("method FindN not implemented")
 }
 
-func (store *ElasticStore) Marshal(object interface{}, project, documentType, id string) error {
-	j, err := json.Marshal(object)
-	if err != nil {
-		return err
-	}
-	return store.Save(project, documentType, id, string(j))
-}
-
-func (store *ElasticStore) Unmarshal(objectRef interface{}, project, documentType string, matcher ...jstore.Matcher) error {
-	j, err := store.Find(project, documentType, matcher...)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal([]byte(j), objectRef)
-}
-
 func (store *ElasticStore) cntx() context.Context {
 	return context.Background()
 }
 
-func (store *ElasticStore) flush(project string) {
-	store.client.Flush(project).Do(store.cntx())
+func (store *ElasticStore) createSearch(project, documentType string, matcher ...jstore.Matcher) *es.SearchService {
+	boolQuery := es.NewBoolQuery()
+	for _, m := range matcher {
+		switch m := m.(type) {
+		case jstore.IdMatcher:
+			boolQuery.Must(es.NewIdsQuery().Ids(m.Value))
+		case jstore.EqMatcher:
+			if _, isString := m.Value.(string); isString {
+				boolQuery.Must(es.NewTermQuery(m.Property+".keyword", m.Value))
+			} else {
+				boolQuery.Must(es.NewTermQuery(m.Property, m.Value))
+			}
+		}
+	}
+
+	return store.client.Search(project).
+		Type(documentType).
+		Index(project).
+		Query(boolQuery)
 }
--- provider.go
+++ provider.go
@@ -1,7 +1,7 @@
 package jstore
 
 // StoreProvider is a factory method for creation of stores.
-type ProviderFunc func(connectionCongig string) (JStore, error)
+type ProviderFunc func(connectionCongig string) (Store, error)
 
 var provider = map[string]ProviderFunc{}
 
